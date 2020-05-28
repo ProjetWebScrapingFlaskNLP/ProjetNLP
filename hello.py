e1833eb11c0f1b00a6cad1986afb5fa899c085e8
Nohossat TRAORE <nohossat.traore@gmail.com>
2020-05-28 14:02:17 +0200

    add new page + start with MVC

--- hello.py
+++ hello.py
@@ -7,6 +7,56 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/presentation')
+def presentation(parts=None):
+    parts = [
+        {
+            'titre':'Introduction',
+            'desc' : 'Verum ad istam omnem orationem brevis est defensio. Nam quoad aetas M. Caeli dare potuit isti suspicioni locum',
+            'link' : '#intro'
+        },
+         {
+            'titre': 'Recuperation des donnees',
+            'desc' : 'Verum ad istam omnem orationem brevis est defensio. Nam quoad aetas M. Caeli dare potuit isti suspicioni locum',
+            'link' : '#scrap'
+        },
+         {
+            'titre':'Preprocessing',
+            'desc' : 'Verum ad istam omnem orationem brevis est defensio. Nam quoad aetas M. Caeli dare potuit isti suspicioni locum',
+            'link' : '#prepro'
+        },
+        {
+            'titre':'Modelisation',
+            'desc' : 'Verum ad istam omnem orationem brevis est defensio. Nam quoad aetas M. Caeli dare potuit isti suspicioni locum',
+            'link' : '#model'
+        },
+        {
+            'titre':'Resultats',
+            'desc' : 'Verum ad istam omnem orationem brevis est defensio. Nam quoad aetas M. Caeli dare potuit isti suspicioni locum',
+            'link' : '#resultat'
+        },
+         {
+            'titre':'Application',
+            'desc' : 'Verum ad istam omnem orationem brevis est defensio. Nam quoad aetas M. Caeli dare potuit isti suspicioni locum',
+            'link' : '#appli'
+        },
+        {
+            'titre':'Conclusion',
+            'desc' : 'Verum ad istam omnem orationem brevis est defensio. Nam quoad aetas M. Caeli dare potuit isti suspicioni locum',
+            'link' : '#conclusion'
+        },
+        {
+            'titre':'Difficultes rencontrees',
+            'desc' : 'Verum ad istam omnem orationem brevis est defensio. Nam quoad aetas M. Caeli dare potuit isti suspicioni locum',
+            'link' : '#difficulte'
+        },
+    ]
+    return render_template('presentation.html', parts=parts)
+
 @app.route('/livredor')
 def livredor():
     return render_template('livredor.html')
